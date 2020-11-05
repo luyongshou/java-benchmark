1a037185d5f88679816ab0b2618d9f5b98fe9b75
louis <luysh@yonyou.com>
2020-11-05 19:59:28 +0800

    添加测试用例

---
+++ src/main/java/com/jsoniter/benchmark/with_long_string/SerEdapJson.java
@@ -0,0 +1,90 @@
+package com.jsoniter.benchmark.with_long_string;
+
+import com.jsoniter.benchmark.All;
+import io.edap.x.io.ByteArrayBufOut;
+import io.edap.x.json.JsonWriter;
+import io.edap.x.json.writer.ByteArrayJsonWriter;
+import org.junit.Test;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : luysh@yonyou.com
+ * @date : 2019/12/25
+ */
+@State(Scope.Thread)
+public class SerEdapJson {
+
+    private TestObject testObject;
+    ByteArrayBufOut out;
+    JsonWriter jw;
+    TestObjectEncoder encoder;
+
+    @Setup(Level.Trial)
+    public void benchSetup(BenchmarkParams params) {
+        testObject = TestObject.createTestObject();
+        out = new ByteArrayBufOut();
+        jw = new ByteArrayJsonWriter(out);
+        encoder = new TestObjectEncoder();
+        encoder.ser(jw, testObject);
+        int len = jw.getPos();
+        byte[] bs = new byte[len];
+        System.arraycopy(out.getWriteBuf().bs, 0, bs, 0, len);
+        System.out.println("length=" + bs.length);
+        System.out.println("+-----------------------------------------------+");
+        System.out.println(new String(bs));
+        System.out.println("+-----------------------------------------------+");
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void ser(Blackhole bh) throws IOException {
+        for (int i = 0; i < 1000; i++) {
+            jw.reset();
+            encoder.ser(jw, testObject);
+            int len = jw.getPos();
+            byte[] bs = new byte[len];
+            System.arraycopy(out.getWriteBuf().bs, 0, bs, 0, len);
+        }
+    }
+
+    public static class TestObjectEncoder {
+
+        static byte[] field1Data = "\"field1\":null".getBytes();
+        public void ser(JsonWriter jw, TestObject object) {
+            jw.writeByte((byte)'{');
+            if (object.field1 == null) {
+                jw.writeBytes(field1Data, 0, 13);
+            } else {
+                jw.writeBytes(field1Data, 0, 9);
+                jw.writeString(object.field1);
+            }
+            jw.writeByte((byte)'}');
+        }
+    }
+
+    @Test
+    public void test() throws IOException {
+        benchSetup(null);
+        assertEquals("31415926", "31415926");
+    }
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        All.loadJMH();
+        Main.main(new String[]{
+                "with_long_string.SerEdapJson",
+                "-i", "5",
+                "-wi", "5",
+                "-f", "1",
+        });
+    }
+}
