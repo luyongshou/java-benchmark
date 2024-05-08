8d928e7bef158ab40e7a366761d7570411e414c4
luysh <luysh@yonyou.com>
2024-05-08 14:05:22 +0800

    增加eproto的压测场景

---
+++ src/main/java/com/jsoniter/benchmark/with_double/DeserEdapProto.java
@@ -0,0 +1,56 @@
+package com.jsoniter.benchmark.with_double;
+
+import com.jsoniter.benchmark.All;
+import io.edap.protobuf.EncodeException;
+import io.edap.protobuf.ProtoBuf;
+import org.junit.Test;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : luysh@yonyou.com
+ * @date : 2019/12/25
+ */
+@State(Scope.Thread)
+public class DeserEdapProto {
+
+    private byte[] testJSON;
+
+    @Setup(Level.Trial)
+    public void benchSetup(BenchmarkParams params) throws EncodeException {
+        testJSON = ProtoBuf.toByteArray(TestObject.createTestObject());
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void deser(Blackhole bh) throws IOException {
+        for (int i = 0; i < 1000; i++) {
+            bh.consume(ProtoBuf.toObject(testJSON, TestObject.class));
+        }
+    }
+
+    @Test
+    public void test() throws IOException, EncodeException {
+        benchSetup(null);
+        assertEquals(31415926, ProtoBuf.toObject(testJSON, TestObject.class).field1);
+    }
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        All.loadJMH();
+        Main.main(new String[]{
+                "with_double.DeserEdapProto",
+                "-i", "5",
+                "-wi", "5",
+                "-f", "1",
+        });
+    }
+}
---
+++ src/main/java/com/jsoniter/benchmark/with_int_list/SerEproto.java
@@ -0,0 +1,78 @@
+package com.jsoniter.benchmark.with_int_list;
+
+import com.jsoniter.benchmark.All;
+import io.edap.eproto.Eproto;
+import io.edap.eproto.EprotoCodecRegister;
+import io.edap.eproto.EprotoEncoder;
+import io.edap.eproto.EprotoWriter;
+import io.edap.eproto.write.ByteArrayWriter;
+import io.edap.io.BufOut;
+import io.edap.protobuf.EncodeException;
+import io.edap.protobuf.ProtoBuf;
+import io.edap.protobuf.internal.ProtoBufOut;
+import org.junit.Test;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import static com.jsoniter.benchmark.All.conver2HexStr;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : luysh@yonyou.com
+ * @date : 2019/12/25
+ */
+@State(Scope.Thread)
+public class SerEproto {
+
+    private TestObject testObject;
+    EprotoEncoder codec;
+    EprotoWriter writer;
+    BufOut out;
+    private ByteArrayOutputStream byteArrayOutputStream;
+
+    @Setup(Level.Trial)
+    public void benchSetup(BenchmarkParams params) {
+        testObject = TestObject.createTestObject();
+        codec = EprotoCodecRegister.instance().getEncoder(testObject.getClass());
+        out    = new ProtoBufOut();
+        writer = new ByteArrayWriter(out);
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] bs = Eproto.toByteArray(testObject);
+        System.out.println("length=" + bs.length);
+        System.out.println("+-----------------------------------------------+");
+        System.out.println(conver2HexStr(bs));
+        System.out.println("+-----------------------------------------------+");
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void ser(Blackhole bh) throws IOException, EncodeException {
+        for (int i = 0; i < 1000; i++) {
+            bh.consume(Eproto.toByteArray(testObject));
+        }
+    }
+
+    @Test
+    public void test() throws IOException {
+        benchSetup(null);
+        assertEquals("31415926", "31415926");
+    }
+
+    public static void main(String[] args) throws IOException, RunnerException {
+        All.loadJMH();
+        Main.main(new String[]{
+                "with_int_list.SerEproto",
+                "-i", "5",
+                "-wi", "5",
+                "-f", "1",
+        });
+    }
+}
